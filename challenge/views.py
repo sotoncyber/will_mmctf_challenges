3e9d953613729b3c1d54991ea27b99c8586e9bde
Und3fin3d <the.william.greenfield@gmail.com>
2025-03-24 16:02:04 +0000

    ruffcheck-fix

--- challenge/views.py
+++ challenge/views.py
@@ -9,25 +9,25 @@ def index() -> str:
     return render_template("index.html")
 
 @app.route('/image', methods=['GET'])
-def ch1():
+def ch1() -> flask.Response:
     return send_from_directory('static', 'will_steg.dmg', as_attachment=True)
 
 @app.route('/DaveIsAGoatedWebmaster', methods=['GET'])
-def step1():
+def step1() -> flask.Response:
     return render_template("step1.html")
 
 @app.route('/DaveIsAGoatedWebmaster/percy2.bmp', methods=['GET'])
-def step2():
+def step2() -> flask.Response:
     return send_from_directory('static', 'percy2.bmp')
 
 @app.route('/crypto', methods=['GET'])
-def ch2():
+def ch2() -> flask.Response:
     return send_from_directory('static','skeleton.py')
 
 @app.route('/bob.jpg', methods=['GET'])
-def bob():
+def bob() -> flask.Response:
     return send_from_directory('static','bob.jpg')
 
 @app.route('/gru.jpg', methods=['GET'])
-def gru():
+def gru() -> flask.Response:
     return send_from_directory('static','gru.jpg')
